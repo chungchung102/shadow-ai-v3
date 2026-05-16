293d22a1eb97e045dd76e55dfef1f9682e2d5c02
chungchung102 <chungchungpro2020@gmail.com>
2026-05-16 18:42:36 +0700

    fix vercel root

--- shadow-ai-v3/frontend/next.config.ts
+++ shadow-ai-v3/frontend/next.config.ts
@@ -1,7 +1,18 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    root: path.join(__dirname),
+  },
 };
 
 export default nextConfig;
